7fe086687d0d34f776a078e88afbd54e59cb0e4e
Steve Gallo <z3gallo@yahoo.com>
2017-01-19 12:49:19 -0500

    Enabled Git markdown mode

--- conf.d/emacs/install.sh
+++ conf.d/emacs/install.sh
@@ -9,5 +9,7 @@ clone_git_repo https://github.com/ejmr/php-mode.git
 clone_git_repo https://github.com/dholm/tabbar.git
 clone_git_repo https://github.com/sellout/emacs-color-theme-solarized.git
 
+# See http://jblevins.org/projects/markdown-mode/
+
 make_dir $SRC_DIR/markdown-mode
 cd $SRC_DIR/markdown-mode && wget http://jblevins.org/projects/markdown-mode/markdown-mode.el
