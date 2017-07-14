382519dba09746692590bb2248d221ed4754a9d2
Steve Gallo <z3gallo@yahoo.com>
2017-07-14 13:27:21 -0400

    Don't keep adding the same code to the bashrc

--- environment/install.sh
+++ environment/install.sh
@@ -5,13 +5,18 @@ log_message "** Installing bash environment"
 backup_file $HOME/.bash_init_env
 backup_file $HOME/.bashrc
 
-cp bash_init_env $HOME/.bash_init_env
-cp -a bash_environment $HOME/.bash_environment
+if [ ! -f $HOME/.bash_init_env ]; then
+    cat <<ENV >>$HOME/.bashrc
 
-cat <<ENV >>$HOME/.bashrc
+# Read environment files
+
 if [ -f $HOME/.bash_init_env ]; then
     . $HOME/.bash_init_env
 fi
 ENV
+fi
+
+cp bash_init_env $HOME/.bash_init_env
+cp -a bash_environment $HOME/.bash_environment
 
 exit 0
