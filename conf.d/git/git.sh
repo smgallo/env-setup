2636785e4daa1071c855806f204023f13ffb2136
Steve Gallo <z3gallo@yahoo.com>
2017-07-14 12:27:03 -0400

    Additional git aliases

--- conf.d/git/git.sh
+++ conf.d/git/git.sh
@@ -1,3 +1,4 @@
-alias git-show-files="git show --pretty='' --name-only"                                                                                                                                                                                                                                   
+alias git-prune-remotes="git remote update origin --prune"
+alias git-show-files="git show --pretty='' --name-only"
 alias git-php-lint="git status|grep 'modified:' | awk '{print \$2;}' | xargs -I} php -l }"                                                                                                                                                                                                
 alias git-phpcs="git status|grep 'modified:' | awk '{print \$2;}' | xargs -I} ~/bin/phpcs --standard=~/src/xdmod/phpcs.xml }"                                                                                                                                                             
