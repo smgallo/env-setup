0d2d99b48f9c74811504435961a6f63ff5ac8e35
Steve Gallo <z3gallo@yahoo.com>
2018-05-11 13:36:55 -0400

    Update vim config and aliases

--- environment/bash_environment/alias.sh
+++ environment/bash_environment/alias.sh
@@ -6,11 +6,9 @@ alias p-redfly="cd ~/Documents/projects/redfly"
 alias emacs="emacs -geometry 120x70"
 alias ls="/bin/ls -FxC"
 alias rm="/bin/rm -i"
-alias h-wingsadm="ssh wingsadm.acsu.buffalo.edu"
-alias h-tgcdb-mirror="ssh 128.205.41.164"
+alias gen-ctags="ctags -R -o .tags --exclude=.git --exclude=vendor --exclude=.diff --exclude=logs --fields=+KSn"
+# Hosts
 alias h-xdcdb-mirror="ssh -A xdcdb-mirror.ccr.xdmod.org"
-alias h-xalt="ssh 199.109.195.72"
 alias h-sciimpact="ssh 199.109.192.233"
 alias h-smgallo-dev='ssh -A smgallo-dev.ccr.xdmod.org'
 alias h-metrics-dev="ssh -A metrics-dev.ccr.buffalo.edu"
-alias h-redflydev="ssh -A 199.109.193.135"
