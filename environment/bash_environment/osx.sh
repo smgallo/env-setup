e2c3f3d2c9a023b371b4fd8052aa37e085c7bcec
Steve Gallo <smgallo@dave.ccr.buffalo.edu>
2018-09-17 12:04:13 -0400

    Add function for using eternal terminal

--- environment/bash_environment/osx.sh
+++ environment/bash_environment/osx.sh
@@ -1,1 +1,12 @@
-alias updatedb="sudo /usr/libexec/locate.updatedb"
+# Connect to a host using Eternal Terminal with a control channel for iTerm2 integration
+# Usage: etmux hostname sessionname
+
+function etmux {
+    host=$1
+    session=$2
+    title="tmux: $2"
+    echo -ne "\033]0;$title\007"
+    et $host -c "tmux -CC new-session -A -s $session"
+}
+
+alias updatedb="/usr/libexec/locate.updatedb"
