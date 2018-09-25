461b0aa22aaa8bfa459eaa880f08c877190b8f94
Steven Gallo <smgallo@buffalo.edu>
2018-09-25 09:21:50 -0400

    Add useful tmux functions

--- environment/bash_environment/alias.sh
+++ environment/bash_environment/alias.sh
@@ -7,8 +7,10 @@ alias emacs="emacs -geometry 120x70"
 alias ls="/bin/ls -FxC"
 alias rm="/bin/rm -i"
 alias gen-ctags="ctags -R -o .tags --exclude=.git --exclude=vendor --exclude=.diff --exclude=logs --fields=+KSn"
-# Hosts
-alias h-xdcdb-backup="ssh -A xdcdb-backup.ccr.xdmod.org"
-alias h-sciimpact="ssh 199.109.192.233"
-alias h-smgallo='ssh -A smgallo.ccr.xdmod.org'
-alias h-metrics-dev="ssh -A metrics-dev.ccr.buffalo.edu"
+# SSH
+alias s-xdcdb-backup="ssh -A xdcdb-backup.ccr.xdmod.org"
+alias s-sciimpact="ssh 199.109.192.233"
+alias s-smgallo='ssh -A smgallo.ccr.xdmod.org'
+alias s-metrics-dev="ssh -A metrics-dev.ccr.buffalo.edu"
+# OSX
+alias updatedb="/usr/libexec/locate.updatedb"
